b18dc5597433f5642d416847359222351a812210
Edson <esalvador032@gmail.com>
2022-10-01 12:39:57 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# RecogPlantify
+ App to recognize plants and their characteristics. It is developed to inform people about their benefits.
---
+++ android/app/src/main/kotlin/com/example/recog_plantify/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.recog_plantify
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
